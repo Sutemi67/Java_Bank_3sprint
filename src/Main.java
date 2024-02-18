c2dbab6e7e1f6cdb121ec234c948d15ae18ece10
sutemi67 <sutemi67@gmail.com>
2024-02-18 17:50:19 +0700

    new ver

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         class BankAccount {
 
-            // создайте переменную int amount. Доступ к переменной должен быть только из наследников и классов в текущем пакете
-            // создайте переменную String currency. Доступ к переменной должен быть только из наследников и классов в текущем пакете
+            protected int amount;
+            protected String currency;
 
             public void replenishBalance(int amount) {
-                // реализуйте метод. Он общий для всех наследников
+                this.amount += amount;
+                System.out.println("Счёт пополнен на " + amount + currency);
             }
 
             public void withdrawCash(int amount) {
@@ -18,82 +19,73 @@ public class Main {
             }
         }
 
-        class DebitAccount /* унаследуйте класс от BankAccount */ {
-
+        class DebitAccount extends BankAccount {
             public DebitAccount(int amount, String currency) {
-                // если amount < 0, вывести сообщение "Баланс дебетового счета не может быть меньше 0"
-                // иначе присвоить переменным amount и currency значения, переданные в конструкторе
+                if (amount < 0) {
+                    System.out.println("Баланс дебетового счета не может быть меньше 0");
+                } else {
+                    this.amount = amount;
+                    this.currency = currency;
+                }
+            }
+
+            @Override
+            public void replenishBalance(int amount) {
+                this.amount += amount;
+                System.out.println("Счёт пополнен на " + amount + currency);
             }
 
             @Override
             public void withdrawCash(int amount) {
-                // если на счету достаточно денег
-                // вычесть amount из amount класса
-                // вывести сообщение "Вы сняли {amount} {currency}"
-                // иначе вывести сообщение "У вас недостаточно средств для снятия суммы {amount} {currency}"
+                if (this.amount >= amount) {
+                    this.amount -= amount;
+                    System.out.println("Вы сняли :" + amount + currency);
+                } else {
+                    System.out.println("У вас недостаточно средств для снятия суммы " + amount + currency);
+                }
+            }
+
+            //@Override
+            public void showBalance() {
+                System.out.println("На вашем счету осталось " + amount + currency);
+            }
+        }
+
+        class CreditAccount extends BankAccount {
+            private int creditLimit;
+
+            public CreditAccount(int amount, String currency, int creditLimit) {
+                this.amount = amount;
+                this.currency = currency;
+                this.creditLimit = creditLimit;
+            }
+
+            /*public void replenishBalance(int amount) {
+                        this.amount += amount;
+                        System.out.println("Счёт пополнен на " + amount + currency);
+                    }
+            */
+            @Override
+            public void windrawCash(int amount) {
+                if (amount > creditLimit) {
+                    this.amount -= amount;
+                    System.out.println("Вы сняли " + amount + currency);
+                } else {
+                    System.out.println("У вас недостаточно средств для снятия суммы " + amount + currency);
+                }
             }
 
             @Override
             public void showBalance() {
-                // вывести сообщение "На вашем счету осталось {amount} {currency}"
+                if (amount > 0) {
+                    System.out.println("На вашем счету осталось " + amount + currency);
+                } else {
+                    System.out.println("Задолженность по кредитному счёту составляет " + amount + currency);
+                }
             }
         }
-
-// создайте класс CreditAccount, который будет наследоваться от класса BankAccount
-
-        // создайте переменную int creditLimit. Переменная должна быть доступна только в пределах класса CreditAccount
-
-        // создайте конструктор, который принимает на вход переменные (int amount, String currency, int creditLimit) и присваивает их значения переменным класса
-
-        // переопределите метод withdrawCash(int amount)
-        // если после снятия наличных будет превышен кредитный лимит
-        // выведите сообщение "У вас недостаточно средств для снятия суммы {amount} {currency}"
-        // иначе уменьшить сумму на счёте и вывести сообщение "Вы сняли {amount} {currency}"
-
-        // переопределите метод showBalance()
-        // если на счету положительная сумма, то выведите сообщение "На вашем счету осталось {amount} {currency}",
-        // иначе "Задолженность по кредитному счёту составляет {amount} {currency}"class BankAccount {
-
-        // создайте переменную int amount. Доступ к переменной должен быть только из наследников и классов в текущем пакете
-        // создайте переменную String currency. Доступ к переменной должен быть только из наследников и классов в текущем пакете
-
-        public void replenishBalance(int amount) {
-            // реализуйте метод. Он общий для всех наследников
-        }
-
-        public void withdrawCash(int amount) {
-        }
-
-        public void showBalance() {
-        }
     }
-
-    class DebitAccount /* унаследуйте класс от BankAccount */ {
-
-        public DebitAccount(int amount, String currency) {
-            // если amount < 0, вывести сообщение "Баланс дебетового счета не может быть меньше 0"
-            // иначе присвоить переменным amount и currency значения, переданные в конструкторе
-        }
-
-        @Override
-        public void withdrawCash(int amount) {
-            // если на счету достаточно денег
-            // вычесть amount из amount класса
-            // вывести сообщение "Вы сняли {amount} {currency}"
-            // иначе вывести сообщение "У вас недостаточно средств для снятия суммы {amount} {currency}"
-        }
-
-        @Override
-        public void showBalance() {
-            // вывести сообщение "На вашем счету осталось {amount} {currency}"
-        }
-    }
-
-// создайте класс CreditAccount, который будет наследоваться от класса BankAccount
-
-// создайте переменную int creditLimit. Переменная должна быть доступна только в пределах класса CreditAccount
-
-// создайте конструктор, который принимает на вход переменные (int amount, String currency, int creditLimit) и присваивает их значения переменным класса
+}
 
 // переопределите метод withdrawCash(int amount)
 // если после снятия наличных будет превышен кредитный лимит
@@ -102,6 +94,7 @@ public class Main {
 
 // переопределите метод showBalance()
 // если на счету положительная сумма, то выведите сообщение "На вашем счету осталось {amount} {currency}",
-// иначе "Задолженность по кредитному счёту составляет {amount} {currency}"
-    }
-}
+// иначе "Задолженность по кредитному счёту составляет {amount} {currency}"class BankAccount {
+
+// создайте переменную int amount. Доступ к переменной должен быть только из наследников и классов в текущем пакете
+// создайте переменную String currency. Доступ к переменной должен быть только из наследников и классов в текущем пакете
